c74488e824664eee3e4f13dfdf32584ec6322b5e
levu <levu@alderaan.scienceco.de>
2012-07-13 02:42:25 +0200

    api result is no generated

--- hmap2apiresult.php
+++ hmap2apiresult.php
@@ -5,5 +5,17 @@
 		$json .= fread(STDIN, 32 * 1024);
 	}
 	$data = json_decode($json, true);
-	echo count($data);
-
+	$result = array();
+	$keys = array_keys($data);
+	foreach($data[$keys[0]] as $k=>$_) {
+		$result[$data['hmapId'][$k]] = array(
+			"title" => "{$data['hmapNameInterv'][$k]} ({$data['hmapRoleInterv'][$k]})",
+			"author" => $data['hmapNameInterv'][$k],
+			"url" => $data['hmapUrlInterv'][$k],
+			"attributes" => array (
+				"startInterv" => $data['hmapStartInterv'][$k],
+				"endInterv" => $data['hmapEndInterv'][$k]
+			)
+		);
+	}
+	echo json_encode($result);
